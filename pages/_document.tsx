128a2e96853358227ccb5fc5e4800685b9f3b584
yuval Styr <yuval.styr@hp.com>
2020-12-03 17:37:04 +0200

    add Meta to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,30 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
 
+const Meta = () => {
+  return (
+    <Head>
+      <title>My page title</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Assistant&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head>
+  )
+}
+
 export default class MyDocument extends Document {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   render() {
     return (
       <Html lang="he">
-        <Head>
-          <title>My page title</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-          <link rel="icon" type="image/png" href="/favicon.png" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Assistant&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Head>
         <body dir="rtl">
+          <Meta />
           <Main />
           <NextScript />
         </body>
