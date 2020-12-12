847f093a0b5331e17e3eca40b2f85cfc33da96cc
yuval Styr <yuval.styr@hp.com>
2020-12-12 19:30:24 +0200

    add email auth providor

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -17,6 +17,17 @@ export default (req: NextApiRequest, res: NextApiResponse): Promise<void> =>
         clientId: process.env.NEXTAUTH_FACEBOOK_ID ?? '',
         clientSecret: process.env.NEXTAUTH_FACEBOOK_SECRET ?? '',
       }),
+      Providers.Email({
+        server: {
+          host: process.env.SMTP_HOST ?? '',
+          port: Number(process.env.SMTP_PORT) ?? '',
+          auth: {
+            user: process.env.SMTP_USER ?? '',
+            pass: process.env.SMTP_PASSWORD ?? '',
+          },
+        },
+        from: process.env.SMTP_FROM, // The "from" address that you want to use
+      }),
     ],
     events: {
       createUser: async (message) => {
